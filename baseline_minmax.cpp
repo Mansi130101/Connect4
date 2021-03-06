479d735d4e7923391d06ad6b57e8287e4cb1d7f3
Mansi130101 <58467251+Mansi130101@users.noreply.github.com>
2021-03-06 14:37:19 +0530

    Update baseline_minmax.cpp

--- baseline_minmax.cpp
+++ baseline_minmax.cpp
@@ -40,32 +40,70 @@ int winning_move(int** board, int piece){
 	# Check horizontal locations for win
 	for (int c = 0; c < COLUMN_COUNT-3; c++)
 		for (int r = 0; r < ROW_COUNT; r++)
-			if (board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece)
+			if (board[r][c] == piece && board[r][c+1] == piece && board[r][c+2] == piece && board[r][c+3] == piece)
 				return 1;
 
 	# Check vertical locations for win
 	for (int c = 0; c < COLUMN_COUNT; c++)
 		for(int r = 0; r < ROW_COUNT-3; r++)
-			if (board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece)
+			if (board[r][c] == piece && board[r+1][c] == piece && board[r+2][c] == piece && board[r+3][c] == piece)
 				return 1;
 
 	# Check positively sloped diaganols
 	for (int c = 0; c < COLUMN_COUNT-3; c++)
 		for (int r = 0; r < ROW_COUNT-3; r++)
-			if (board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece)
+			if (board[r][c] == piece && board[r+1][c+1] == piece && board[r+2][c+2] == piece && board[r+3][c+3] == piece)
 				return 1;
 
 	# Check negatively sloped diaganols
 	for (int c = 0; c < COLUMN_COUNT-3; c++)
 		for (int r = 3; r < ROW_COUNT; r++)
-			if (board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece)
+			if (board[r][c] == piece && board[r-1][c+1] == piece && board[r-2][c+2] == piece && board[r-3][c+3] == piece)
 				return 1;
   
   	return 0;
 }
 
 int main(){
-  	board = create_board();
-  
+  	int** board;
+	int row, col;
+	board = create_board();
+	print_board(board);
+	int game_over = 0, turn = 0;
+  	while (!game_over){
+		// Ask for Player 1 Input
+		if turn == 0{
+			printf("Player 1: Select a column form 0-6");
+			scanf("%d",&col);
+
+			if is_valid_location(board, col){
+				row = get_next_open_row(board, col);
+				drop_piece(board, row, col, 1);
+				if winning_move(board, 1){
+					printf("Player 1 wins!!\n");
+					game_over = 1;
+				}
+			}
+		}
+		// Ask for Player 2 Input
+		else{			
+			printf("Player 2: Select a column form 0-6");
+			scanf("%d",&col);
+
+			if (is_valid_location(board, col)){
+				row = get_next_open_row(board, col);
+				drop_piece(board, row, col, 2);
+				if winning_move(board, 1){
+					printf("Player 2 wins!!\n");
+					game_over = 1;
+				}
+			}
+		}
+		print_board(board);
+		turn += 1;
+		turn = turn % 2;
+		return 0;
+	}
+	printf("GAME OVER\n");
   	free(board);
  }
