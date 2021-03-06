cf228bf07a58628f497c8949287719edb392c8e8
Mansi130101 <58467251+Mansi130101@users.noreply.github.com>
2021-03-06 14:23:49 +0530

    Update baseline_minmax.cpp

--- baseline_minmax.cpp
+++ baseline_minmax.cpp
@@ -1,16 +1,16 @@
 #incluse<iostream>
 #include <stdio.h> 
 #include <stdlib.h> 
-#incluse<math.h>
+#include<math.h>
 using namespace std;
 
 int ROW_COUNT = 6;
 int COLUMN_COUNT = 7;
 
 int** create_board(){
-  int** board = (int **)calloc(ROW_COUNT, sizeof(int *));
-  for (int i=0; i<ROW_COUNT; i++)
-         board[i] = (int *)calloc(COLUMN_COUNT, sizeof(int));
+  	int** board = (int **)calloc(ROW_COUNT, sizeof(int *));
+  	for (int i=0; i<ROW_COUNT; i++)
+        	board[i] = (int *)calloc(COLUMN_COUNT, sizeof(int));
 	return board;
 }
 
@@ -20,8 +20,8 @@ void drop_piece(int** board, int row, int col, int piece){
 
 int is_valid_location(int** board, int col){
 	if (board[ROW_COUNT-1][col] == 0)
-    return 1;
-  return 0;
+    		return 1;
+  	return 0;
 }
 
 int get_next_open_row(int** board, int col){
@@ -32,8 +32,8 @@ int get_next_open_row(int** board, int col){
 
 void print_board(int** board){
 	for (int i = 0; i <  ROW_COUNT; i++)
-      for (int j = COLUMN_COUNT-1; j > -1; j--)
-         printf("%d ", *(board + i*COLUMN_COUNT + j));
+      		for (int j = COLUMN_COUNT-1; j > -1; j--)
+         		printf("%d ", *(board + i*COLUMN_COUNT + j));
 }
 
 int winning_move(int** board, int piece){
@@ -61,11 +61,11 @@ int winning_move(int** board, int piece){
 			if (board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece)
 				return 1;
   
-  return 0;
+  	return 0;
 }
 
 int main(){
-  board = create_board();
+  	board = create_board();
   
-  free(board);
+  	free(board);
  }
